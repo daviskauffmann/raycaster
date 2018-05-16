74e1f08c4a3485c9e56c8f19a67dc2dea2b18009
Davis Kauffmann <daviskauffmann@gmail.com>
2018-05-16 03:27:36 -0400

    Update build script

--- build_x86.bat
+++ build_x86.bat
@@ -7,9 +7,7 @@ set LINKER_OPTS=/libpath:lib\x86\ /subsystem:console
 set LIBS=SDL2_image.lib SDL2_mixer.lib SDL2_ttf.lib SDL2.lib SDL2main.lib SDL2test.lib
 
 del bin\x86\*.exe
-del bin\x86\*.exp
 del bin\x86\*.ilk
-del bin\x86\*.lib
 del bin\x86\*.pdb
 
 rd build\x86\ /s /q 
